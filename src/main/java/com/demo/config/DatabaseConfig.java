f4be6a96cbaed5b94c78276736d97eadd583fa20
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:40:13 +0700

    add hibernate config

--- src/main/java/com/demo/config/DatabaseConfig.java
+++ src/main/java/com/demo/config/DatabaseConfig.java
@@ -25,7 +25,6 @@ public class DatabaseConfig {
 		hikari.setJdbcUrl(url);
 		hikari.setUsername("postgres");
 		hikari.setPassword(password);
-		hikari.setDriverClassName("postgresql");
 		return hikari;
 	}
 }
