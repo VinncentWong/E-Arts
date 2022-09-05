0a29abe86be584b9ac7a304edcf6703725556eec
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:12:18 +0700

    Add data source config

---
+++ src/main/java/com/demo/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.demo.config;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+	
+	@Value("${SUPABASE_URL}")
+	private String url;
+	
+	@Value("${SUPABASE_PASSWORD}")
+	private String password;
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariDataSource hikari = new HikariDataSource();
+		hikari.setConnectionTimeout(3000);
+		hikari.setJdbcUrl(url);
+		hikari.setUsername("postgres");
+		hikari.setPassword(password);
+		hikari.setDriverClassName("postgresql");
+		return hikari;
+	}
+}
