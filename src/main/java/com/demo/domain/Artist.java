1a1472a81d887b2902c08251ea13ea69d293cf98
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 11:37:46 +0700

    add artist domain

---
+++ src/main/java/com/demo/domain/Artist.java
@@ -0,0 +1,23 @@
+package com.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Artist {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String about;
+	
+	private String biography;
+}
