bdb06e5387abd500ada129489f891a8a8d13a0eb
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 11:45:33 +0700

    Add education domain

---
+++ src/main/java/com/demo/domain/Educational.java
@@ -0,0 +1,25 @@
+package com.demo.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Educational {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String education;
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Artist artistId;
+}
