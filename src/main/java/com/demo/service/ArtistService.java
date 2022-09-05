692b918e8c901873c9f88e1d810a2034af19b931
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 14:13:45 +0700

    add artist service content

--- src/main/java/com/demo/service/ArtistService.java
+++ src/main/java/com/demo/service/ArtistService.java
@@ -2,20 +2,41 @@ package com.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.demo.domain.Artist;
+import com.demo.domain.PersonalInformation;
 import com.demo.domain.Response;
 import com.demo.domain.dto.SignUpDto;
 import com.demo.repositories.ArtistRepository;
+import com.demo.repositories.PersonalInformationRepository;
+import com.demo.util.ResponseUtil;
 
 @Service
 public class ArtistService {
 
-	private final ArtistRepository repository;
+	private final PersonalInformationRepository repository;
+	
+	private final BCryptPasswordEncoder bcrypt;
+	
+	private final ResponseUtil util;
 	
 	@Autowired
-	public ArtistService(ArtistRepository repository) {
+	public ArtistService(PersonalInformationRepository repository, BCryptPasswordEncoder bcrypt, ResponseUtil util) {
 		this.repository = repository;
+		this.bcrypt = bcrypt;
+		this.util = util;
+	}
+	
+	public ResponseEntity<Response> createArtist(SignUpDto dto){
+		Artist artist = new Artist();
+		artist.setEmail(dto.getEmail());
+		artist.setPassword(dto.getPassword());
+		PersonalInformation tempPersonal = new PersonalInformation();
+		tempPersonal.setDateBirth(dto.getBirthDate());
+		tempPersonal.setArtist(artist);
+		var personal = repository.save(tempPersonal);
+		return util.sendCreated("success create user data", true, personal);
 	}
 }
