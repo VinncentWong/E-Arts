56f7b471e790e923445dec2c3dc7aca21be3c63b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 13:15:30 +0700

    add depedency

--- src/main/java/com/demo/security/provider/JwtProvider.java
+++ src/main/java/com/demo/security/provider/JwtProvider.java
@@ -1,5 +1,26 @@
 package com.demo.security.provider;
 
-public class JwtProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
+import com.demo.security.authentication.JwtAuthentication;
+
+public class JwtProvider implements AuthenticationProvider{
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		if(authentication.equals(JwtAuthentication.class)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
