4e3edd1797948c2335450f47206ca3fc19471fd8
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 13:57:02 +0700

    add bcrypt

--- src/main/java/com/demo/service/ArtistService.java
+++ src/main/java/com/demo/service/ArtistService.java
@@ -1,8 +1,12 @@
 package com.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.demo.domain.Artist;
+import com.demo.domain.Response;
+import com.demo.domain.dto.SignUpDto;
 import com.demo.repositories.ArtistRepository;
 
 @Service
@@ -14,5 +18,4 @@ public class ArtistService {
 	public ArtistService(ArtistRepository repository) {
 		this.repository = repository;
 	}
-	
 }
