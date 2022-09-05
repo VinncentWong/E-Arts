96f3e66ed8d06ca30acf452d2f4fb20d16dbe93c
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 13:24:17 +0700

    Add jwt provider content

--- src/main/java/com/demo/security/provider/JwtProvider.java
+++ src/main/java/com/demo/security/provider/JwtProvider.java
@@ -1,17 +1,43 @@
 package com.demo.security.provider;
 
+import java.util.ArrayList;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.demo.security.authentication.JwtAuthentication;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 public class JwtProvider implements AuthenticationProvider{
+	
+	@Value("${JWT_SECRET_KEY}")
+	private String secretKey;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+		String token = authentication.getName();
+		try {
+			Claims claims = Jwts.parser()
+							.setSigningKey(Base64.getEncoder().encodeToString(secretKey.getBytes()))
+							.parseClaimsJws(token)
+							.getBody();
+			String role = claims.get("ROLE").toString();
+			role = new String("ROLE_" + role);
+			var auth = new ArrayList<GrantedAuthority>();
+			auth.add(new SimpleGrantedAuthority(role));
+			return new JwtAuthentication(token, null, auth);
+			
+		}
+		catch(Exception ex) {
+			return new JwtAuthentication("tidak valid", null);
+		}
 	}
 
 	@Override
