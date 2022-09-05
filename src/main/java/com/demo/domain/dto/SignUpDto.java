a41b1e0d6452e0a128c87c87c83db01b2816de35
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:58:10 +0700

    add signup dto

---
+++ src/main/java/com/demo/domain/dto/SignUpDto.java
@@ -0,0 +1,35 @@
+package com.demo.domain.dto;
+
+import java.util.Date;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.demo.domain.Role;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+	
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	private String name;
+	
+	@NotNull
+	@NotBlank
+	private String password;
+	
+	@NotNull
+	private Date birthDate;
+	
+	@Enumerated(EnumType.STRING)
+	@NotNull
+	private Role role;
+}
