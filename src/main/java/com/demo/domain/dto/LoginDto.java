37ef5aa0c309a7cbaa03d7ac900322d8e2bd20a2
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:58:53 +0700

    add login dto

---
+++ src/main/java/com/demo/domain/dto/LoginDto.java
@@ -0,0 +1,18 @@
+package com.demo.domain.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	private String password;
+}
