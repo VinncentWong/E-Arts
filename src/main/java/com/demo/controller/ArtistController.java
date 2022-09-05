ac560a78d38159c97858ce5745ee5cf05f2ffd4b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 14:24:01 +0700

    add artist controller

---
+++ src/main/java/com/demo/controller/ArtistController.java
@@ -0,0 +1,31 @@
+package com.demo.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.domain.Response;
+import com.demo.domain.dto.SignUpDto;
+import com.demo.service.ArtistService;
+
+@RestController
+@RequestMapping("/artist")
+public class ArtistController {
+	
+	private final ArtistService service;
+	
+	@Autowired
+	public ArtistController(ArtistService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/create")
+	public ResponseEntity<Response> createUser(@RequestBody @Valid SignUpDto dto){
+		return service.createArtist(dto);
+	}
+}
