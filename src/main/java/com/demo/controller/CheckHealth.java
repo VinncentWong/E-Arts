50a391b702a4c1ff4ead985630aaf61f997139e3
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:43:20 +0700

    add checkhealth implementation

--- src/main/java/com/demo/controller/CheckHealth.java
+++ src/main/java/com/demo/controller/CheckHealth.java
@@ -1,5 +1,13 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CheckHealth {
 
+	@GetMapping("/checkhealth")
+	public String checkHealth() {
+		return "OK";
+	}
 }
