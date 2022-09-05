56219cf6e36756e8c362dc541111e6acf7603a70
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:00:20 +0700

    Add more properties

--- src/main/java/com/demo/domain/Artist.java
+++ src/main/java/com/demo/domain/Artist.java
@@ -1,9 +1,16 @@
 package com.demo.domain;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -15,9 +22,24 @@ public class Artist {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	private String email;
+	
+	private String password;
+	
 	private String name;
 	
 	private String about;
 	
 	private String biography;
+	
+	@CreationTimestamp
+	@Temporal(TemporalType.TIME)
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIME)
+	private Date updatedAt;
+	
+	@Temporal(TemporalType.TIME)
+	private Date deletedAt;
 }
