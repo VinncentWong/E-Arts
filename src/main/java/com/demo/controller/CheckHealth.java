2469dc98be5b7383694eb1da1a234be7fcbb6b69
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 15:51:30 +0700

    add .env to .gitignore

--- src/main/java/com/demo/controller/CheckHealth.java
+++ src/main/java/com/demo/controller/CheckHealth.java
@@ -1,5 +1,8 @@
 package com.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckHealth {
 
 	@GetMapping("/checkhealth")
-	public String checkHealth() {
-		return "OK";
+	public Map<String, Object> checkHealth() {
+		var map = new HashMap<String, Object>();
+		map.put("message", "OK");
+		return map;
 	}
 }
