dfc000d83fc45d04cc88312e2554e0d8e47f28dd
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-05 12:47:58 +0700

    add educational repository and @repository

--- src/main/java/com/demo/repositories/EducationalRepository.java
+++ src/main/java/com/demo/repositories/EducationalRepository.java
@@ -3,7 +3,7 @@ package com.demo.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.demo.domain.Awards;
+import com.demo.domain.Educational;
 
 @Repository
-public interface AwardRepository extends CrudRepository<Awards, Long>{}
+public interface EducationalRepository extends CrudRepository<Educational, Long>{}
