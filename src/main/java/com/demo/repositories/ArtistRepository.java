5606762017cd5c9273de4b949b254006990e7b97
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 10:20:45 +0700

    add artist getByEmail

--- src/main/java/com/demo/repositories/ArtistRepository.java
+++ src/main/java/com/demo/repositories/ArtistRepository.java
@@ -1,9 +1,19 @@
 package com.demo.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.Artist;
 
 @Repository
-public interface ArtistRepository extends CrudRepository<Artist, Long>{}
+public interface ArtistRepository extends CrudRepository<Artist, Long>{
+	
+	/*
+	 * Mendapatkan data artis melalui email
+	 */
+	@Query(nativeQuery = true, value = "SELECT * FROM artist WHERE email = ?1")
+	Optional<Artist> getArtistByEmail(String email);
+}
