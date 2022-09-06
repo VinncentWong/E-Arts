1e6d4b0b9537fa43607cf9f0b373c2e68a50664f
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 11:17:33 +0700

    add personan information repo more content

--- src/main/java/com/demo/repositories/PersonalInformationRepository.java
+++ src/main/java/com/demo/repositories/PersonalInformationRepository.java
@@ -1,9 +1,18 @@
 package com.demo.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.PersonalInformation;
 
 @Repository
-public interface PersonalInformationRepository extends CrudRepository<PersonalInformation, Long>{}
+public interface PersonalInformationRepository extends CrudRepository<PersonalInformation, Long>{
+	/*
+	 * Get personal information data by artist id
+	 */
+	@Query(nativeQuery = true, value = "SELECT * FROM personal_information WHERE artist_id = ?1")
+	Optional<PersonalInformation> getPersonalInformationByArtistId(Long id);
+}
