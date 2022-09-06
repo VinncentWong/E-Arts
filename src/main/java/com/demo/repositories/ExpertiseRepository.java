4032a5815ca1a0afa3d3ff1d6dc4d24b62753e1f
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 15:34:21 +0700

    add expertise more method

--- src/main/java/com/demo/repositories/ExpertiseRepository.java
+++ src/main/java/com/demo/repositories/ExpertiseRepository.java
@@ -3,6 +3,7 @@ package com.demo.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,8 @@ public interface ExpertiseRepository extends CrudRepository<Expertise, Long> {
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM expertise WHERE artist_id = ?1")
 	Optional<List<Expertise>> getExpertisesByArtistId(Long artistId);
+	
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE expertise SET expertise.expertise = ?1 WHERE id = ?2")
+	void updateExpertiseData(String text, Long expertiseId);
 }
