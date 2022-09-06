a04264ad8d1822af0c1df495bf991090f6f85bd6
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 13:38:30 +0700

    use json ignore

--- src/main/java/com/demo/domain/Educational.java
+++ src/main/java/com/demo/domain/Educational.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Entity
@@ -21,5 +23,6 @@ public class Educational {
 	private String education;
 	
 	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JsonIgnore
 	private Artist artistId;
 }
