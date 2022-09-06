84456a269ead5e1d7ba239639cd822e9bac993b6
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 10:08:11 +0700

    add jwt util implementation

--- src/main/java/com/demo/util/JwtUtil.java
+++ src/main/java/com/demo/util/JwtUtil.java
@@ -1,12 +1,23 @@
 package com.demo.util;
 
+import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.demo.domain.Human;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @Component
-public class JwtUtil<T> {
+public class JwtUtil<T extends Human> {
+	
+	@Value("${JWT_SECRET_KEY}")
+	private String secretKey;
 	
 	public String generateToken(IJwt<T> jwt, T obj) {
 		return jwt.generateToken(obj);
@@ -14,6 +25,14 @@ public class JwtUtil<T> {
 	
 	public String implementationGenerateToken(T obj) {
 		Map<String, Object> claims = new HashMap<>();
-		return "";
+		claims.put("id", obj.getId());
+		claims.put("name", obj.getName());
+		claims.put("createdAt", obj.getCreatedAt());
+		String token = Jwts.builder()
+							.setClaims(claims)
+							.signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(secretKey.getBytes()))
+							.setExpiration(new Date(System.currentTimeMillis() + 1000000L))
+							.compact();
+		return token;
 	}
 }
