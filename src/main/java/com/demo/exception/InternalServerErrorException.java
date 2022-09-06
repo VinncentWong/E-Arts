51c4ec9d3bb98fe4f82629d65023551d1bebf28f
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 10:27:21 +0700

    add internalservererror exception

---
+++ src/main/java/com/demo/exception/InternalServerErrorException.java
@@ -0,0 +1,15 @@
+package com.demo.exception;
+
+public class InternalServerErrorException extends Exception{
+
+	public InternalServerErrorException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InternalServerErrorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
