0aa9afab6736373c08475638a2c4f9e1e111e053
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 14:30:15 +0700

    add expertise repository content

--- src/main/java/com/demo/repositories/ExpertiseRepository.java
+++ src/main/java/com/demo/repositories/ExpertiseRepository.java
@@ -1,9 +1,17 @@
 package com.demo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.Expertise;
 
 @Repository
-public interface ExpertiseRepository extends CrudRepository<Expertise, Long> {}
+public interface ExpertiseRepository extends CrudRepository<Expertise, Long> {
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM expertise WHERE artist_id = ?1")
+	Optional<List<Expertise>> getExpertisesByArtistId(Long artistId);
+}
