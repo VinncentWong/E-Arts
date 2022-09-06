73d94597d6278ca649d5ecca4c8173607821ed4d
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 10:13:00 +0700

    add artistnotfound exception

---
+++ src/main/java/com/demo/exception/ArtistNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+public class ArtistNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5061550646418757332L;
+
+	public ArtistNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArtistNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
