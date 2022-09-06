60071b975d6a03ff219815ed6ca1381b009cc0cf
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 10:26:34 +0700

    add more content

--- src/main/java/com/demo/service/ArtistService.java
+++ src/main/java/com/demo/service/ArtistService.java
@@ -10,8 +10,10 @@ import com.demo.domain.PersonalInformation;
 import com.demo.domain.Response;
 import com.demo.domain.dto.LoginDto;
 import com.demo.domain.dto.SignUpDto;
+import com.demo.exception.ArtistNotFoundException;
 import com.demo.repositories.ArtistRepository;
 import com.demo.repositories.PersonalInformationRepository;
+import com.demo.util.JwtUtil;
 import com.demo.util.ResponseUtil;
 
 @Service
@@ -24,12 +26,15 @@ public class ArtistService {
 	private final ResponseUtil util;
 	
 	private final ArtistRepository artistRepo;
+	
+	private final JwtUtil<Artist> jwtUtil;
 	@Autowired
-	public ArtistService(PersonalInformationRepository repository, BCryptPasswordEncoder bcrypt, ResponseUtil util, ArtistRepository artistRepo) {
+	public ArtistService(PersonalInformationRepository repository, BCryptPasswordEncoder bcrypt, ResponseUtil util, ArtistRepository artistRepo, JwtUtil<Artist> jwtUtil) {
 		this.repository = repository;
 		this.bcrypt = bcrypt;
 		this.util = util;
 		this.artistRepo = artistRepo;
+		this.jwtUtil = jwtUtil;
 	}
 	
 	public ResponseEntity<Response> createArtist(SignUpDto dto){
@@ -43,7 +48,13 @@ public class ArtistService {
 		return util.sendCreated("success create user data", true, artist);
 	}
 	
-	public ResponseEntity<Response> loginArtist(LoginDto dto){
-		
+	public ResponseEntity<Response> loginArtist(LoginDto dto) throws ArtistNotFoundException{
+		Artist artist = artistRepo.getArtistByEmail(dto.getEmail()).orElseThrow(() -> new ArtistNotFoundException());
+		if(bcrypt.matches(dto.getPassword(), artist.getPassword())) {
+			String token = jwtUtil.generateToken(jwtUtil::implementationGenerateToken, artist);
+			return util.sendOk("user authenticated", true, token);
+		} else {
+			throw new InternalServerErrorException();
+		}
 	}
 }
