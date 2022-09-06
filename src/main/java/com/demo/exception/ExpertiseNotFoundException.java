ba7287c9c1d79a3118ad085302c1a02c3151b00b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-06 15:05:21 +0700

    add expertise not found exception

---
+++ src/main/java/com/demo/exception/ExpertiseNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+public class ExpertiseNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7893090852210988852L;
+
+	public ExpertiseNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExpertiseNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
