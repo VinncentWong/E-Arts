cbfb4a6439aa8ad15434c0adf6aa08f6ebf3e7c2
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-07 09:20:08 +0700

    Add awardsnotfound exception

---
+++ src/main/java/com/demo/exception/AwardsNotFoundException.java
@@ -0,0 +1,19 @@
+package com.demo.exception;
+
+public class AwardsNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7354306316059562638L;
+	
+	public AwardsNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AwardsNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
