3e86ff5c84fbaab134a152de61691fd20a689e56
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-07 09:11:11 +0700

    Add constraint

--- src/main/java/com/demo/domain/dto/ExpertiseDto.java
+++ src/main/java/com/demo/domain/dto/ExpertiseDto.java
@@ -1,10 +1,15 @@
 package com.demo.domain.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class ExpertiseDto {
 
+	@NotNull
+	@NotBlank
 	private String text;
 	
 }
