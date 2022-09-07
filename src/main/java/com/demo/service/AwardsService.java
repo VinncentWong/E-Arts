72f1894066a58330fc0e43990b102beedc19ab71
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-07 09:17:56 +0700

    add awards service content

--- src/main/java/com/demo/service/AwardsService.java
+++ src/main/java/com/demo/service/AwardsService.java
@@ -2,10 +2,14 @@ package com.demo.service;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.demo.domain.Artist;
 import com.demo.domain.Response;
+import com.demo.domain.dto.AwardsDto;
+import com.demo.repositories.ArtistRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @Slf4j
 public class AwardsService {
+	
+	private final ArtistRepository artistRepo;
+	
+	@Autowired
+	public AwardsService(ArtistRepository artistRepo) {
+		this.artistRepo = artistRepo;
+	}
 
+	public ResponseEntity<Response> createAwards(AwardsDto dto, Long artistId){
+		Artist artist = artistRepo.findById(artistId).orElseThrow();
+		return null;
+	}
 	
 }
