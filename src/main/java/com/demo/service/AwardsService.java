238bc2d92e3014c68cf3da25fc39d4bb4c14055e
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-07 11:52:14 +0700

    add awards service

--- src/main/java/com/demo/service/AwardsService.java
+++ src/main/java/com/demo/service/AwardsService.java
@@ -7,9 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.demo.domain.Artist;
+import com.demo.domain.Awards;
 import com.demo.domain.Response;
 import com.demo.domain.dto.AwardsDto;
+import com.demo.exception.ArtistNotFoundException;
+import com.demo.exception.AwardsNotFoundException;
 import com.demo.repositories.ArtistRepository;
+import com.demo.repositories.AwardRepository;
+import com.demo.util.ResponseUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,14 +25,34 @@ public class AwardsService {
 	
 	private final ArtistRepository artistRepo;
 	
+	private final AwardRepository awardsRepo;
+	
+	private final ResponseUtil util;
+	
 	@Autowired
-	public AwardsService(ArtistRepository artistRepo) {
+	public AwardsService(ArtistRepository artistRepo, ResponseUtil util, AwardRepository awardsRepo) {
 		this.artistRepo = artistRepo;
+		this.util = util;
+		this.awardsRepo = awardsRepo;
 	}
 
-	public ResponseEntity<Response> createAwards(AwardsDto dto, Long artistId){
-		Artist artist = artistRepo.findById(artistId).orElseThrow();
-		return null;
+	public ResponseEntity<Response> createAwards(AwardsDto dto, Long artistId) throws AwardsNotFoundException{
+		Artist artist = artistRepo.findById(artistId).orElseThrow(() -> new AwardsNotFoundException());
+		Awards awards = new Awards();
+		awards.setArtistId(artist);
+		return util.sendCreated("success create awards", true, awards);
 	}
 	
+	public ResponseEntity<Response> getAwards(Long awardsId, Long artistId)
+			throws ArtistNotFoundException, AwardsNotFoundException{
+		artistRepo.findById(artistId).orElseThrow(() -> new ArtistNotFoundException());
+		Awards awards = awardsRepo.findById(awardsId).orElseThrow(() -> new AwardsNotFoundException());
+		return util.sendOk("success find awards data", true, awards);
+	}
+	
+	public ResponseEntity<Response> updateAwards(Long awardsId, Long artistId, AwardsDto dto)
+			throws ArtistNotFoundException, AwardsNotFoundException{
+		artistRepo.findById(artistId).orElseThrow(() -> new ArtistNotFoundException());
+		return null;
+	}
 }
