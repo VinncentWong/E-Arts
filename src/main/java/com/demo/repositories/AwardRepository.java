6438d1a66decad8af4cf404f3e3fc09aa4275f7a
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-07 11:58:30 +0700

    add awards repository content

--- src/main/java/com/demo/repositories/AwardRepository.java
+++ src/main/java/com/demo/repositories/AwardRepository.java
@@ -1,9 +1,19 @@
 package com.demo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.Awards;
 
 @Repository
-public interface AwardRepository extends CrudRepository<Awards, Long>{}
+public interface AwardRepository extends CrudRepository<Awards, Long>{
+	
+	@Modifying
+	@Query(nativeQuery = true, value = "SELECT * FROM awards WHERE artist_id = ?1")
+	Optional<List<Awards>> getAwardsByArtistId(Long artistId);
+}
