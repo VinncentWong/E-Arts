c65cc74de53a5638128b1db5c93cea46f16697fa
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-08 20:54:16 +0700

    add more exception handler

--- src/main/java/com/demo/aspect/CustomControllerAdvice.java
+++ src/main/java/com/demo/aspect/CustomControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.demo.domain.Response;
 import com.demo.exception.ArtistNotFoundException;
+import com.demo.exception.AwardsNotFoundException;
 import com.demo.exception.ExpertiseNotFoundException;
 import com.demo.exception.InternalServerErrorException;
 import com.demo.util.ResponseUtil;
@@ -34,5 +35,10 @@ public class CustomControllerAdvice {
 	@ExceptionHandler(ExpertiseNotFoundException.class)
 	public ResponseEntity<Response> sendExpertiseNotFoundException(){
 		return util.sendInternalServerError("data expertise tidak ditemukan", false);
+	}
+	
+	@ExceptionHandler(AwardsNotFoundException.class)
+	public ResponseEntity<Response> sendAwardsNotFoundException(){
+		return util.sendInternalServerError("data awards tidak ditemukan", false);
 	}
 }
