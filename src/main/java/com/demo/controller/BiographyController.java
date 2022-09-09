b3f9390c9f5aab3390de9c2b72ec6134f3e71e07
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-09 13:02:15 +0700

    Add biography controller

---
+++ src/main/java/com/demo/controller/BiographyController.java
@@ -0,0 +1,54 @@
+package com.demo.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.domain.Response;
+import com.demo.domain.dto.BiographyDto;
+import com.demo.exception.ArtistNotFoundException;
+import com.demo.service.BiographyService;
+
+@RestController
+@RequestMapping("/biography")
+public class BiographyController {
+
+	private final BiographyService service;
+	
+	@Autowired
+	public BiographyController(BiographyService service) {
+		this.service = service;
+	}
+	
+	@PostMapping("/create/{artistId}")
+	public ResponseEntity<Response> createBiography(@RequestBody @Valid BiographyDto dto, @PathVariable("id") Long id) throws ArtistNotFoundException{
+		return this.service.createBiography(id, dto);
+	}
+	
+	@GetMapping("/get/{artistId}")
+	public ResponseEntity<Response> getBiography(@PathVariable("artistId") Long artistId) throws ArtistNotFoundException{
+		return this.service.getBiography(artistId);
+	}
+	
+	@PatchMapping("/update/{artistId}")
+	public ResponseEntity<Response> updateBiography(@PathVariable("artistId") Long artistId, @RequestBody @Valid BiographyDto dto)
+				throws ArtistNotFoundException{
+		return this.service.updateBiography(artistId, dto);
+	}
+	
+	@DeleteMapping("/delete/{artistId}")
+	public ResponseEntity<Response> deleteBiography(@PathVariable("artistId") Long artistId) throws ArtistNotFoundException{
+		return this.service.deleteBiography(artistId);
+	}
+	
+	
+}
