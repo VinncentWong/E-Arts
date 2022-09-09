c921f920c4097c39b631297d20d168b1a55814ec
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-09 10:43:51 +0700

    add biography service

--- src/main/java/com/demo/service/BiographyService.java
+++ src/main/java/com/demo/service/BiographyService.java
@@ -1,13 +1,24 @@
 package com.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.demo.domain.Response;
+import com.demo.domain.dto.BiographyDto;
+import com.demo.repositories.ArtistRepository;
 
 @Service
 public class BiographyService {
 	
+	private final ArtistRepository repository;
+	
 	@Autowired
-	public BiographyService() {
-		
+	public BiographyService(ArtistRepository repository) {
+		this.repository = repository;
+	}
+	
+	public ResponseEntity<Response> createBiography(Long artistId, BiographyDto dto){
+		return null;
 	}
 }
