c53b98338eac4d1645ed829c4c1cbfe87710755a
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-09 09:59:59 +0700

    change method name

--- src/main/java/com/demo/repositories/ArtistRepository.java
+++ src/main/java/com/demo/repositories/ArtistRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.Artist;
+import com.demo.domain.dto.AboutDto;
 
 @Repository
 public interface ArtistRepository extends CrudRepository<Artist, Long>{
@@ -17,6 +18,6 @@ public interface ArtistRepository extends CrudRepository<Artist, Long>{
 	@Query(nativeQuery = true, value = "SELECT * FROM artist WHERE email = ?1")
 	Optional<Artist> getArtistByEmail(String email);
 	
-	@Query(nativeQuery = true, value = "SELECT about FROM artist WHERE id = ?1")
-	Optional<Artist> getArtistAbout(Long artistId);
+	@Query(nativeQuery = true, value = "UPDATE artist SET about = ?1 WHERE id = ?2")
+	Optional<Artist> updateArtistAbout(String about, Long artistId);
 }
