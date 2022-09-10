004ef7af28e9a89416ff20c4d29e7534d62cc815
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-09-10 14:36:03 +0700

    add query on educational repository

--- src/main/java/com/demo/repositories/EducationalRepository.java
+++ src/main/java/com/demo/repositories/EducationalRepository.java
@@ -1,9 +1,17 @@
 package com.demo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.Educational;
 
 @Repository
-public interface EducationalRepository extends CrudRepository<Educational, Long>{}
+public interface EducationalRepository extends CrudRepository<Educational, Long>{
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM educational WHERE artist_id_id = ?1")
+	Optional<List<Educational>> getEducationalsByArtistId(Long artistId);
+}
