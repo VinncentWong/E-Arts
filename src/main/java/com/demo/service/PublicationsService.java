820c96f579a2d0e3a77eae5521964b0f5cb19aaf
unknown <vinncentwong2@gmail.com>
2022-09-12 12:50:12 +0700

    add add publication method

--- src/main/java/com/demo/service/PublicationsService.java
+++ src/main/java/com/demo/service/PublicationsService.java
@@ -4,20 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.demo.domain.Artist;
+import com.demo.domain.Publications;
 import com.demo.domain.Response;
+import com.demo.domain.dto.PublicationDto;
+import com.demo.exception.ArtistNotFoundException;
+import com.demo.repositories.ArtistRepository;
 import com.demo.repositories.PublicationRepository;
+import com.demo.util.ResponseUtil;
 
 @Service
 public class PublicationsService {
 
 	private final PublicationRepository repository;
 	
+	private final ArtistRepository artistRepository;
+	
+	private final ResponseUtil util;
+	
 	@Autowired
-	public PublicationsService(PublicationRepository repository) {
+	public PublicationsService(PublicationRepository repository, ArtistRepository artistRepository, ResponseUtil util) {
 		this.repository = repository;
+		this.artistRepository = artistRepository;
+		this.util = util;
 	}
 	
-	public ResponseEntity<Response> createPublication(){
-		return null;
+	public ResponseEntity<Response> createPublication(PublicationDto dto, Long artistId) throws ArtistNotFoundException{
+		Artist artist = this.artistRepository.findById(artistId).orElseThrow(() -> new ArtistNotFoundException());
+		Publications publication = new Publications();
+		publication.setPublications(dto.getPublication());
+		publication.setArtist(artist);
+		this.repository.save(publication);
+		return util.sendCreated("sukses membuat publication", true, artist);
 	}
 }
