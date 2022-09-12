2d3ac2b99815290a60852d83a2a80c48817196b3
unknown <vinncentwong2@gmail.com>
2022-09-12 12:51:20 +0700

    Add publication not found exception

---
+++ src/main/java/com/demo/exception/PublicationNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+public class PublicationNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9188702450497509701L;
+
+	public PublicationNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicationNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
