2e7db4012ca8e0923755ddff4e1aca19586ba03c
unknown <vinncentwong2@gmail.com>
2022-09-13 10:47:11 +0700

    add social media repository

--- src/main/java/com/demo/repositories/SocialMediaRepository.java
+++ src/main/java/com/demo/repositories/SocialMediaRepository.java
@@ -1,5 +1,10 @@
 package com.demo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +13,17 @@ import com.demo.domain.SocialMedia;
 @Repository
 public interface SocialMediaRepository extends CrudRepository<SocialMedia, Long>{
 	
+	@Query(nativeQuery = true, value = "SELECT * FROM social_media WHERE artist_id = ?1")
+	public Optional<List<SocialMedia>> getSocialMedias(Long artistId);
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM social_media WHERE artist_id = ?1 AND id = ?2")
+	public Optional<SocialMedia> getSocialMedia(Long artistId, Long socialMediaId);
+	
+	@Query(nativeQuery = true, value = "UPDATE social_media SET social_media = ?3 WHERE id = ?2 AND artist_id = ?1")
+	@Modifying
+	public void updateSocialMedia(Long artistId, Long socialMediaId, String socialMedia);
+	
+	@Query(nativeQuery = true, value = "DELETE FROM social_media WHERE artist_id = ?1 AND id = ?2")
+	@Modifying
+	public void deleteSocialMedia(Long artistId, Long socialMediaId);
 }
