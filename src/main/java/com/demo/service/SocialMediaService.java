4e5b26e21f88045c8b91de109ca55473d9e0fbee
unknown <vinncentwong2@gmail.com>
2022-09-13 14:25:38 +0700

    Add transactional

--- src/main/java/com/demo/service/SocialMediaService.java
+++ src/main/java/com/demo/service/SocialMediaService.java
@@ -2,6 +2,8 @@ package com.demo.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@Transactional
 public class SocialMediaService {
 
 	private final SocialMediaRepository repository;
