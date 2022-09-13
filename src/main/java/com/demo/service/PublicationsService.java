121eeb01f3568a91a9b47ff5eb8a129c2d5f750d
unknown <vinncentwong2@gmail.com>
2022-09-13 10:15:29 +0700

    Add transactional

--- src/main/java/com/demo/service/PublicationsService.java
+++ src/main/java/com/demo/service/PublicationsService.java
@@ -2,6 +2,8 @@ package com.demo.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ import com.demo.repositories.PublicationRepository;
 import com.demo.util.ResponseUtil;
 
 @Service
+@Transactional
 public class PublicationsService {
 
 	private final PublicationRepository repository;
