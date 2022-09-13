d052668ed2f2e1c62683033d4b8a20866dcb78fc
unknown <vinncentwong2@gmail.com>
2022-09-13 15:28:26 +0700

    add personal information not found exception

---
+++ src/main/java/com/demo/exception/PersonalInformationNotFoundException.java
@@ -0,0 +1,19 @@
+package com.demo.exception;
+
+public class PersonalInformationNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8864652133548318243L;
+
+	public PersonalInformationNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonalInformationNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
