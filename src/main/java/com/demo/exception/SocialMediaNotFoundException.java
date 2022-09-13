c6d5903beaa0cb6fafb8ac93c08f98364129f2d6
unknown <vinncentwong2@gmail.com>
2022-09-13 10:28:46 +0700

    add social media not found exception

---
+++ src/main/java/com/demo/exception/SocialMediaNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+
+public class SocialMediaNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -415826319742125503L;
+
+	public SocialMediaNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SocialMediaNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
