a070cb2ee3ca3bc78190ecae1803f65a0a2811ce
unknown <vinncentwong2@gmail.com>
2022-09-13 10:14:37 +0700

    add modifying

--- src/main/java/com/demo/repositories/PublicationRepository.java
+++ src/main/java/com/demo/repositories/PublicationRepository.java
@@ -3,6 +3,7 @@ package com.demo.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -20,8 +21,10 @@ public interface PublicationRepository extends CrudRepository<Publications, Long
 	public Optional<Publications> getPublication(Long artistId, Long publicationId);
 	
 	@Query(nativeQuery = true, value = "UPDATE publications SET publications = ?3 WHERE artist_id = ?1 AND id = ?2")
+	@Modifying
 	public void updatePublication(Long artistId, Long publicationId, String publications);
 	
 	@Query(nativeQuery = true, value = "DELETE FROM publications WHERE id = ?2 AND artist_id = ?1")
+	@Modifying
 	public void deletePublication(Long artistId, Long publicationId);
 }
