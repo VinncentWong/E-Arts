9a2f0a251831c7c05874156b045253c45948a956
unknown <vinncentwong2@gmail.com>
2022-09-13 09:20:24 +0700

    add get publication

--- src/main/java/com/demo/repositories/PublicationRepository.java
+++ src/main/java/com/demo/repositories/PublicationRepository.java
@@ -14,4 +14,7 @@ public interface PublicationRepository extends CrudRepository<Publications, Long
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM publications")
 	public Optional<List<Publications>> getPublications(Long id);
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM publications WHERE artist_id = ?1 AND id = ?2")
+	public Publications getPublication(Long artistId, Long publicationId);
 }
