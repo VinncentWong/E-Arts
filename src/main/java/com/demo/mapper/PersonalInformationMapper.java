ff23b8879a79033c3ae3bf9592889d6521e6bad9
unknown <vinncentwong2@gmail.com>
2022-09-14 14:55:48 +0700

    add mapper

---
+++ src/main/java/com/demo/mapper/PersonalInformationMapper.java
@@ -0,0 +1,7 @@
+package com.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.dynamic.sql.util.mybatis3.CommonUpdateMapper;
+
+@Mapper
+public interface PersonalInformationMapper extends CommonUpdateMapper{}
