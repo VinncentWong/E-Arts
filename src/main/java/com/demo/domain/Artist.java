4d42fd5b92679309ed7574b84ae2fd7c31b53d55
unknown <vinncentwong2@gmail.com>
2022-09-15 10:11:35 +0700

    remove unused import

--- src/main/java/com/demo/domain/Artist.java
+++ src/main/java/com/demo/domain/Artist.java
@@ -17,8 +17,6 @@ import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.Data;
 import lombok.ToString;
 
