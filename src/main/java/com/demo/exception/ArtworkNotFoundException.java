66fdbb531dfbb304f0831a6d0327928bb778b5d6
unknown <vinncentwong2@gmail.com>
2022-09-16 12:14:35 +0700

    add artwork not found exception

---
+++ src/main/java/com/demo/exception/ArtworkNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+public class ArtworkNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5975623995096410360L;
+
+	public ArtworkNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArtworkNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
