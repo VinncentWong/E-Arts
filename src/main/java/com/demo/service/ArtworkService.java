3a23d50203c3cd3b9ebccdc00befa281d88c83b0
unknown <vinncentwong2@gmail.com>
2022-09-16 12:05:11 +0700

    Add create artwork

--- src/main/java/com/demo/service/ArtworkService.java
+++ src/main/java/com/demo/service/ArtworkService.java
@@ -3,14 +3,24 @@ package com.demo.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.demo.domain.Response;
+import com.demo.repositories.ArtworkRepository;
 
 @Service
 @Transactional
 public class ArtworkService {
 
+	private final ArtworkRepository repository;
+	
 	@Autowired
-	public ArtworkService() {
-		
+	public ArtworkService(ArtworkRepository repository) {
+		this.repository = repository;
+	}
+	
+	public ResponseEntity<Response> createArtwork(){
+		return null;
 	}
 }
