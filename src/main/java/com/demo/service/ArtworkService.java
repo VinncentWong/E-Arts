a23fe5bc1803f67f6b33cc0602ee5cb4db63abf3
unknown <vinncentwong2@gmail.com>
2022-09-16 12:10:11 +0700

    Add search artist

--- src/main/java/com/demo/service/ArtworkService.java
+++ src/main/java/com/demo/service/ArtworkService.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.demo.domain.ArtWork;
+import com.demo.domain.Artist;
 import com.demo.domain.Response;
+import com.demo.domain.dto.ArtworkDto;
+import com.demo.exception.ArtistNotFoundException;
+import com.demo.repositories.ArtistRepository;
 import com.demo.repositories.ArtworkRepository;
 
 @Service
@@ -15,12 +20,17 @@ public class ArtworkService {
 
 	private final ArtworkRepository repository;
 	
+	private final ArtistRepository artistRepository;
+	
 	@Autowired
-	public ArtworkService(ArtworkRepository repository) {
+	public ArtworkService(ArtworkRepository repository, ArtistRepository artistRepository) {
 		this.repository = repository;
+		this.artistRepository = artistRepository;
 	}
 	
-	public ResponseEntity<Response> createArtwork(){
+	public ResponseEntity<Response> createArtwork(ArtworkDto dto, Long artistId) throws ArtistNotFoundException{
+		Artist artist = this.artistRepository.findById(artistId).orElseThrow(() -> new ArtistNotFoundException());
+		ArtWork artwork = new ArtWork();
 		return null;
 	}
 }
