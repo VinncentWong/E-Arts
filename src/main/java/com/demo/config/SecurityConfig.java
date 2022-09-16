fe642a5ade7c2aec4f9514dcb772eaba451a6da9
unknown <vinncentwong2@gmail.com>
2022-09-16 11:42:12 +0700

    add artwork

--- src/main/java/com/demo/config/SecurityConfig.java
+++ src/main/java/com/demo/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 		.addFilterBefore(filter(), UsernamePasswordAuthenticationFilter.class)
 		.authorizeHttpRequests()
 		.anyRequest()
-		.authenticated()
+		.permitAll()
 		.and()
 		.formLogin().disable()
 		.httpBasic().disable();
