496f40c68fd777913952c563f8b993aed83571a6
unknown <vinncentwong2@gmail.com>
2022-09-19 17:58:18 +0700

    add photo not found exception

---
+++ src/main/java/com/demo/exception/PhotoNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+public class PhotoNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4781671013671884540L;
+
+	public PhotoNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PhotoNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
