94e3cc9b4e891fb4d1c4671ea3f97ec56eb0c03e
unknown <vinncentwong2@gmail.com>
2022-09-20 16:43:44 +0700

    Add get user by email

--- src/main/java/com/demo/repositories/UserRepository.java
+++ src/main/java/com/demo/repositories/UserRepository.java
@@ -1,9 +1,16 @@
 package com.demo.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{}
+public interface UserRepository extends CrudRepository<User, Long>{
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM user WHERE email = ?1 LIMIT 1")
+	Optional<User> getUserByEmail(String email);
+}
