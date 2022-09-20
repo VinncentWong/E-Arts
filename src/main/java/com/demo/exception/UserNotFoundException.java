9f510600b099ba9199d65d5e53821609b82aaab6
unknown <vinncentwong2@gmail.com>
2022-09-20 16:33:28 +0700

    Add user not found exception

---
+++ src/main/java/com/demo/exception/UserNotFoundException.java
@@ -0,0 +1,20 @@
+package com.demo.exception;
+
+public class UserNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 444080816009921732L;
+
+	public UserNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
