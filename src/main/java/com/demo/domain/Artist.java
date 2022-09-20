7f451e1f4197f7b262e9e89ce6fd21cb5d5a3bb0
unknown <vinncentwong2@gmail.com>
2022-09-20 16:20:49 +0700

    add siugnupuser dto

--- src/main/java/com/demo/domain/Artist.java
+++ src/main/java/com/demo/domain/Artist.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -39,6 +41,7 @@ public class Artist implements Human{
 	
 	private String biography;
 	
+	@Enumerated(EnumType.STRING)
 	private Role role;
 	
 	@CreationTimestamp
---
+++ src/main/java/com/demo/domain/dto/SignUpUserDto.java
@@ -0,0 +1,36 @@
+package com.demo.domain.dto;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.demo.domain.Role;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class SignUpUserDto {
+
+	@Email
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	private String username;
+	
+	@NotNull
+	@NotBlank
+	private String password;
+	
+	@NotNull
+	private Date birthDate;
+	
+	@NotNull
+	private Role role;
+}
