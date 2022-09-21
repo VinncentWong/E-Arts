5389415ca8c0ff322e4afffd4597e2abab77f1dd
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-21 22:16:44 +0700

    Add user dto

---
+++ src/main/java/com/demo/domain/dto/UserDto.java
@@ -0,0 +1,33 @@
+package com.demo.domain.dto;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.demo.domain.Gender;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @NotNull
+    @NotBlank
+    private String password;
+
+    @NotNull
+    @NotBlank
+    private String email;
+
+    @NotNull
+    @NotBlank
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Gender male;
+}
