270743720ff302ab2dcd5d515fe0d228dd19c74b
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-21 17:59:54 +0700

    add table name

--- src/main/java/com/demo/domain/User.java
+++ src/main/java/com/demo/domain/User.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
-@Entity
+@Entity(name = "users")
+@Table(name = "users")
 @Data
 public class User {
 	
