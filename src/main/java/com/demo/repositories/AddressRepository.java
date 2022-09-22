95126c68280d6aff03b8077e039bf7748534c113
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-22 11:57:44 +0700

    add getOneAddress

--- src/main/java/com/demo/repositories/AddressRepository.java
+++ src/main/java/com/demo/repositories/AddressRepository.java
@@ -1,7 +1,14 @@
 package com.demo.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.demo.domain.user.Address;
 
-public interface AddressRepository extends CrudRepository<Address, Long>{}
+public interface AddressRepository extends CrudRepository<Address, Long>{
+
+    @Query(nativeQuery = true, value = "SELECT * FROM address WHERE id = ?2 AND user_id = ?1")
+    Optional<Address> getOneAddress(Long userId, Long addressId);
+}
