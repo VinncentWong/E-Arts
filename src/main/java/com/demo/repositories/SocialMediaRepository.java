246523023561f4eacf60227ea81e65fcee9ab12b
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-22 11:03:49 +0700

    remove unused import

--- src/main/java/com/demo/repositories/SocialMediaRepository.java
+++ src/main/java/com/demo/repositories/SocialMediaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.demo.domain.SocialMedia;
+import com.demo.domain.artist.SocialMedia;
 
 @Repository
 public interface SocialMediaRepository extends CrudRepository<SocialMedia, Long>{
--- src/main/java/com/demo/repositories/UserRepository.java
+++ src/main/java/com/demo/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.demo.domain.User;
+import com.demo.domain.user.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
