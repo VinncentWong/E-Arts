1fbc58eab8b27dca1cef974fc66cd65ba876713b
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-22 10:04:42 +0700

    remove unused import

--- src/main/java/com/demo/service/SocialMediaService.java
+++ src/main/java/com/demo/service/SocialMediaService.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.demo.domain.Artist;
 import com.demo.domain.Response;
-import com.demo.domain.SocialMedia;
+import com.demo.domain.artist.Artist;
+import com.demo.domain.artist.SocialMedia;
 import com.demo.domain.dto.SocialMediaDto;
 import com.demo.exception.ArtistNotFoundException;
 import com.demo.exception.SocialMediaNotFoundException;
