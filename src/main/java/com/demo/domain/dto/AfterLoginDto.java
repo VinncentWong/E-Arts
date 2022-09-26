fc0a137d53c62801acf1de60d7bbb4b124cf233e
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-26 15:41:37 +0700

    Add @Data

--- src/main/java/com/demo/domain/dto/AfterLoginDto.java
+++ src/main/java/com/demo/domain/dto/AfterLoginDto.java
@@ -3,6 +3,9 @@ package com.demo.domain.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
+@Data
 public class AfterLoginDto {
     
     @NotNull
