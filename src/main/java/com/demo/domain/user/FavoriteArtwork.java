3ec71403637d62d1ecffe80a79dd30a4fd951100
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-27 16:36:16 +0700

    Add favorite artwork

---
+++ src/main/java/com/demo/domain/user/FavoriteArtwork.java
@@ -0,0 +1,22 @@
+package com.demo.domain.user;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class FavoriteArtwork {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+}
