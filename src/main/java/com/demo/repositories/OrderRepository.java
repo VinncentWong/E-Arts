1107c198d4dc1206b260683da0dcfec1fed799d9
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-28 13:46:20 +0700

    add order repository

---
+++ src/main/java/com/demo/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.domain.user.Order;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long>{}
