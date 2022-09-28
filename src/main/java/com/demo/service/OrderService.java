edfe780c86b1c6510062fabb991ab8aa9ca377ba
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-28 14:14:53 +0700

    add orderservice

---
+++ src/main/java/com/demo/service/OrderService.java
@@ -0,0 +1,52 @@
+package com.demo.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.demo.domain.Response;
+import com.demo.domain.artist.ArtWork;
+import com.demo.domain.dto.OrderDto;
+import com.demo.domain.user.Order;
+import com.demo.domain.user.User;
+import com.demo.exception.ArtworkNotFoundException;
+import com.demo.exception.UserNotFoundException;
+import com.demo.repositories.ArtworkRepository;
+import com.demo.repositories.OrderRepository;
+import com.demo.repositories.UserRepository;
+import com.demo.util.ResponseUtil;
+
+@Service
+@Transactional
+public class OrderService {
+    
+    private final OrderRepository orderRepository;
+
+    private final ArtworkRepository artworkRepository;
+
+    private final UserRepository userRepository;
+
+    private final ResponseUtil util;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository, ArtworkRepository artworkRepository, UserRepository userRepository, ResponseUtil util){
+        this.orderRepository = orderRepository;
+        this.artworkRepository = artworkRepository;
+        this.userRepository = userRepository;
+        this.util = util;
+    }
+
+    public ResponseEntity<Response> createOrder(Long artworkId, Long userId, OrderDto dto) throws ArtworkNotFoundException, UserNotFoundException{
+        ArtWork artwork = this.artworkRepository.findById(artworkId).orElseThrow(() -> new ArtworkNotFoundException());
+        User user = this.userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        Order order = new Order();
+        order.setArtwork(artwork);
+        order.setUser(user);
+        order.setQuantity(dto.getQuantity());
+        order.setTotalPrice(dto.getTotalPrice());
+        this.orderRepository.save(order);
+        return this.util.sendCreated("sukses membuat order", true, order);
+    }
+}
