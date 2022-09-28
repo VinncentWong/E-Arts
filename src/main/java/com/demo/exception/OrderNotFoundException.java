e9ce5b6625c858f0c8d3312f74e6674e99b69d1f
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-28 13:45:08 +0700

    add order not found exception

---
+++ src/main/java/com/demo/exception/OrderNotFoundException.java
@@ -0,0 +1,12 @@
+package com.demo.exception;
+
+public class OrderNotFoundException extends Exception{
+
+    public OrderNotFoundException() {
+    }
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+    
+}
