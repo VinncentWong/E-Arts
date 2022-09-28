3a49d15f591870281da83dac5fed68c3454cbca7
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-28 13:41:30 +0700

    add order.java

---
+++ src/main/java/com/demo/domain/user/Order.java
@@ -0,0 +1,27 @@
+package com.demo.domain.user;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.demo.domain.artist.ArtWork;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ArtWork artwork;
+}
