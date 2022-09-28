7c72d4d200a13e3ba7c393bf5b21938c1d627d4c
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-28 14:14:37 +0700

    remove properti

--- src/main/java/com/demo/domain/dto/OrderDto.java
+++ src/main/java/com/demo/domain/dto/OrderDto.java
@@ -2,19 +2,10 @@ package com.demo.domain.dto;
 
 import javax.validation.constraints.NotNull;
 
-import com.demo.domain.artist.ArtWork;
-import com.demo.domain.user.User;
-
 import lombok.Data;
 
 @Data
 public class OrderDto {
-    
-    @NotNull
-    private User user;
-
-    @NotNull
-    private ArtWork artwork;
 
     @NotNull
     private Integer quantity;
