ee2900de73167297dce4cafcefa0d32c9222d283
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-29 12:54:30 +0700

    Add order method

--- src/main/java/com/demo/repositories/OrderRepository.java
+++ src/main/java/com/demo/repositories/OrderRepository.java
@@ -1,9 +1,20 @@
 package com.demo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.user.Order;
 
 @Repository
-public interface OrderRepository extends CrudRepository<Order, Long>{}
+public interface OrderRepository extends CrudRepository<Order, Long>{
+
+    @Query(nativeQuery = true, value = "SELECT * FROM order WHERE order = ?1")
+    Optional<List<Order>> getOrders(Long artistId);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM order WHERE user_id = ?1 AND id = ?2")
+    Optional<Order> getOrder(Long userId, Long orderId);
+}
