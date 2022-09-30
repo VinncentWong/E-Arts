9c607c6478b6ff3b9dd792d2678cd99218284192
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-30 08:36:21 +0700

    Add get user

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,11 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<Response> login(@RequestBody @Valid LoginDto dto){
         return this.service.login(dto);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getUser(@PathVariable("id") Long id) throws UserNotFoundException{
+        return this.service.getUser(id);
     }
 
     @PatchMapping("/update/{userId}")
