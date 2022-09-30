cd4f669ebf71e9cb9fcf27fcde91380b92766340
Vinncent Wong <vinncentwong2@gmail.com>
2022-09-30 08:17:22 +0700

    Add mapping

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -4,7 +4,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,18 +30,22 @@ public class UserController {
         this.service = service;
     }
 
+    @PostMapping("/create")
     public ResponseEntity<Response> createUser(@RequestBody @Valid SignUpDto dto){
         return this.service.createUser(dto);
     }
 
+    @PostMapping("/login")
     public ResponseEntity<Response> login(@RequestBody @Valid LoginDto dto){
         return this.service.login(dto);
     }
 
+    @PatchMapping("/update/{userId}")
     public ResponseEntity<Response> updateUser(@RequestBody @Valid UserDto dto, @PathVariable("userId") Long userId) throws UserNotFoundException{
         return this.service.updateUser(userId, dto);
     }   
 
+    @DeleteMapping("/delete/{userId}")
     public ResponseEntity<Response> deleteUser(@PathVariable("userId") Long userId) throws UserNotFoundException{
         return this.service.deleteUser(userId);
     }
