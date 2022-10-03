9b0538896f07df36f32ab778ea4c9171c4d2b32f
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-03 12:34:48 +0700

    add property

--- src/main/java/com/demo/domain/user/FavoriteArtwork.java
+++ src/main/java/com/demo/domain/user/FavoriteArtwork.java
@@ -1,11 +1,16 @@
 package com.demo.domain.user;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.demo.domain.artist.ArtWork;
 
@@ -21,4 +26,10 @@ public class FavoriteArtwork {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private ArtWork artwork;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
 }
