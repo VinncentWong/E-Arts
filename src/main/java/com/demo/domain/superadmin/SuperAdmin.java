3933f8e6a9404e763f4c3fb73c4c1366e35a8c41
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-10 12:30:55 +0700

    add identifier

--- src/main/java/com/demo/domain/superadmin/SuperAdmin.java
+++ src/main/java/com/demo/domain/superadmin/SuperAdmin.java
@@ -1,6 +1,9 @@
 package com.demo.domain.superadmin;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -11,6 +14,8 @@ import lombok.Data;
 @Entity
 public class SuperAdmin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String username;
