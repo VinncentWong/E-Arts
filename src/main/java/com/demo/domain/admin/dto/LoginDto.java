a27b8cc879afc57e93c4d3b9dd5828174843dd7b
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-10 09:55:59 +0700

    Add not null and not blank

--- src/main/java/com/demo/domain/admin/dto/LoginDto.java
+++ src/main/java/com/demo/domain/admin/dto/LoginDto.java
@@ -1,11 +1,18 @@
 package com.demo.domain.admin.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class LoginDto {
     
+    @NotNull
+    @NotBlank
     private String phoneNumber;
 
+    @NotNull
+    @NotBlank
     private String password;
 }
