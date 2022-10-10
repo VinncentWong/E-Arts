e15e4bd3ae3edac07fd0162d45e4fa98b3ec280d
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-10 13:05:08 +0700

    add admin not found exception

---
+++ src/main/java/com/demo/exception/AdminNotFoundException.java
@@ -0,0 +1,12 @@
+package com.demo.exception;
+
+public class AdminNotFoundException extends Exception{
+
+    public AdminNotFoundException() {
+    }
+
+    public AdminNotFoundException(String message) {
+        super(message);
+    }
+    
+}
