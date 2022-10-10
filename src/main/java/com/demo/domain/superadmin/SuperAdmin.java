ecbfa028e97850598830e4ded349a381a2a50414
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-10 13:27:51 +0700

    implements human

--- src/main/java/com/demo/domain/superadmin/SuperAdmin.java
+++ src/main/java/com/demo/domain/superadmin/SuperAdmin.java
@@ -3,6 +3,8 @@ package com.demo.domain.superadmin;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,11 +12,14 @@ import javax.persistence.Id;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.demo.domain.Human;
+import com.demo.domain.Role;
+
 import lombok.Data;
 
 @Data
 @Entity
-public class SuperAdmin {
+public class SuperAdmin implements Human{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +28,9 @@ public class SuperAdmin {
     private String username;
 
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @CreationTimestamp
     private Date createdAt;
