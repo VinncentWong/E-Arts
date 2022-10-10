da98c4b864a88513b60a4d638af646e2164a4dae
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-10 13:18:36 +0700

    Add super admin repo

--- src/main/java/com/demo/repositories/SuperAdminRepository.java
+++ src/main/java/com/demo/repositories/SuperAdminRepository.java
@@ -1,9 +1,16 @@
 package com.demo.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.demo.domain.superadmin.SuperAdmin;
 
 @Repository
-public interface SuperAdminRepository extends CrudRepository<SuperAdmin, Long>{}
+public interface SuperAdminRepository extends CrudRepository<SuperAdmin, Long>{
+
+    @Query(nativeQuery = true, value = "SELECT * FROM super_admin WHERE username = ?1")
+    Optional<SuperAdmin> getSuperAdminByUsername(String username);
+}
