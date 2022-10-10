eb5b33a916f648ee4d5ff0155744c2bf3b508603
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-10 12:10:29 +0700

    add create admin service

---
+++ src/main/java/com/demo/service/SuperAdminService.java
@@ -0,0 +1,42 @@
+package com.demo.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.demo.domain.Response;
+import com.demo.domain.Role;
+import com.demo.domain.admin.dto.LoginDto;
+import com.demo.domain.admin.entity.Admin;
+import com.demo.repositories.AdminRepository;
+import com.demo.util.ResponseUtil;
+
+@Service
+@Transactional
+public class SuperAdminService {
+    
+    private final AdminRepository repository;
+
+    private final BCryptPasswordEncoder bcrypt;
+
+    private final ResponseUtil util;
+
+    @Autowired
+    public SuperAdminService(AdminRepository repository, BCryptPasswordEncoder bcrypt, ResponseUtil util){
+        this.repository = repository;
+        this.bcrypt = bcrypt;
+        this.util = util;
+    }
+
+    public ResponseEntity<Response> createAdmin(LoginDto dto){
+        Admin admin = new Admin();
+        admin.setPhoneNumber(dto.getPhoneNumber());
+        admin.setRole(Role.ADMIN);
+        admin.setPassword(bcrypt.encode(dto.getPassword()));
+        this.repository.save(admin);
+        return this.util.sendCreated("sukses membuat admin", true, admin);
+    }
+}
